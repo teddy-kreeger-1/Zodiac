cf20b972ea17e67d60f681915c05dec955ed6633
Ted Kreeger <teddy.kreeger.1@student.prcc.edu>
2013-02-14 21:48:33 -0600

    added new files from chapter 3

---
+++ inc_home_links_bar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<html>
+  <head>
+    <title>Styled nested list example 4</title>
+    <link type="text/css" rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+  <div id="wrapper">
+    <ul id="nav">
+       <li><a href="#">PHP</a></li>
+       <li><a href="#">Chinese Zodiac</a></li>
+
+    </ul>
+  </div>
+  
+
+  </body>
+</html>
---
+++ inc_php_info.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Chinese Zodiac</title>
+</head>
+<body>
+
+
+	<?php
+include ("includes/inc_links_bar.php")
+
+
+	?>
+  <h1>PHP-What's it good for?</h1>
+
+	<p>PHP is a server side scripting language. That means its processing happens in the server by consuming server’s resources and sends only the output to the client. PHP can also be used as a language for Command line Scripting and Desktop applications. But it’s widely used for web applications.</p> 
+
+	<p>PHP Web Development Makes Well-Organized and lucrative Websites. PHP is a coding technology invented for making applications, forms, and more as well as web development and can be easily set in into HTML. PHP technology runs on a web server, the programming of PHP technology proceeds as the input and output is the formation of the web forms. The coding tool is also applied for command-line programming and client-side GUI apps. It has been organized on scores of web servers, platforms and OS systems. It is also functional with scores of DBMS. The inclusive resource is on hand to the people at no cost. The website owners or developers can make; alter codes as per their standard uses.</php>
+
+
+
+
+</body>
+
+</html>
